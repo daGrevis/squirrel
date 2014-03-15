db374d1cc7188b4102ff727d5edfe458b6533584
Raitis Stengrevics <dagrevis@gmail.com>
2014-03-15 16:37:39 +0200

    Fix markdown plugin so it's run only when needed

--- markdown_for_squirrel/__init__.py
+++ markdown_for_squirrel/__init__.py
@@ -1,9 +1,25 @@
 import markdown
 
+import helpers
+
+
+logger = helpers.get_logger(__name__)
+
 
 def markdownify_content(context):
-    for article in context["articles"]:
-        article["content"] = markdown.markdown(article["content"])
+    if context["is_called_from_cli"]:
+
+        arg_parser = context["arg_parser"]
+        args = arg_parser.parse_args()
+
+        if args.action == "generate":
+
+            for article in context["articles"]:
+                article["content"] = markdown.markdown(article["content"])
+
+                logger.debug("Parsing content to Markdown for `{}` article..."
+                            .format(article["title"]))
+
     return context
 
 
